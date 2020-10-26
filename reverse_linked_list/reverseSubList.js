4ea291efe87bd53ef5cd8ba3d907e8a551fda5be
dcampuzano101 <dcampuzano101@gmail.com>
2020-10-26 17:49:56 -0400

    reverse sublist

--- reverse_linked_list/reverseSubList.js
+++ reverse_linked_list/reverseSubList.js
@@ -17,4 +17,34 @@ const reverseSubList = (head, p, q) => {
     current = current.next;
     i += 1;
   }
+
+  // we are interested in three parts of the LinkedList, the part before index 'p',
+  // the part between 'p' and 'q', and the part after index 'q'
+  const last_node_of_first_part = previous;
+  // after reversing the LinkedList 'current' will become the last node of the sub-list
+  const last_node_of_sub_list = current;
+  let next = null; // will be used to temporarily store the next node
+
+  i = 0;
+  // reverse nodes between 'p' and 'q'
+  while (current !== null && i < q - p + 1) {
+    next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+    i += 1;
+  }
+
+  // connect with the first part
+  if (last_node_of_first_part !== null) {
+    // 'previous' is now the first node of the sub-list
+    last_node_of_first_part.next = previous;
+    // this means p === 1 i.e., we are changing the first node (head) of the LinkedList
+  } else {
+    head = previous;
+  }
+
+  // connect with the last part
+  last_node_of_sub_list.next = current;
+  return head;
 };
