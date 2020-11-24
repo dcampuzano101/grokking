40a07731d3518eda15d8e10091bde42b06a0e16c
dcampuzano101 <dcampuzano101@gmail.com>
2020-11-24 14:50:13 -0500

    algoexpert bstValidate

---
+++ algoexpert/bstValidate.js
@@ -0,0 +1,20 @@
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const validateBst = (tree) => {
+  return validateBstHelper(tree, -Infinity, Infinity);
+};
+
+const validateBstHelper = (tree, minValue, maxValue) => {
+  if (tree === null) return true;
+  if (tree.value < minValue || tree.value >= maxValue) return false;
+
+  const leftIsValid = validateBstHelper(tree.left, minValue, tree.value);
+
+  return leftIsValid && validateBstHelper(tree.right, tree.value, maxValue);
+};
