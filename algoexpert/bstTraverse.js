3159bdd1868efaf2b50c9e61f54b696c9315ebb4
dcampuzano101 <dcampuzano101@gmail.com>
2020-11-24 15:00:19 -0500

    algoexpert bstTraverse inOrder preOrder postOrder

---
+++ algoexpert/bstTraverse.js
@@ -0,0 +1,34 @@
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const inOrderTraverse = (tree, array) => {
+  if (tree !== null) {
+    inOrderTraverse(tree.left, array);
+    array.push(tree.value);
+    inOrderTraverse(tree.right, array);
+  }
+  return array;
+};
+
+const preOrderTraverse = (tree, array) => {
+  if (tree !== null) {
+    array.push(tree.value);
+    preOrderTraverse(tree.left, array);
+    preOrderTraverse(tree.right, array);
+  }
+  return array;
+};
+
+const postOrderTraverse = (tree, array) => {
+  if (tree !== null) {
+    postOrderTraverse(tree.left, array);
+    postOrderTraverse(tree.right, array);
+    array.push(tree.value);
+  }
+  return array;
+};
