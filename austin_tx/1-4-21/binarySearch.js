4981bfd1bf89df097dd225aed51ea24a8359b21e
dcampuzano101 <dcampuzano101@gmail.com>
2021-01-04 10:17:28 -0600

    bunch more algos ;

--- austin_tx/1-4-21/binarySearch.js
+++ austin_tx/1-4-21/binarySearch.js
@@ -4,7 +4,7 @@ const binarySearch = (array, target) => {
   let midPoint = Math.floor(array.length / 2);
 
   if (target > array[midPoint]) {
-    let subResult = binarySearch(array.slice(midPoint + 1), target);
+    let subResult = binarySearch(array.slice(midPoint), target);
     return subResult === -1 ? -1 : midPoint + 1 + subResult;
   } else if (target < array[midPoint]) {
     return binarySearch(array.slice(0, midPoint), target);
