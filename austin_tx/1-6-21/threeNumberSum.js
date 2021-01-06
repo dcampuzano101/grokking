aab69d6eb64ba528ecfd1078dfb20e0318b91da1
dcampuzano101 <dcampuzano101@gmail.com>
2021-01-06 09:48:29 -0600

    redo - algoexpert - medium - threeSum+smallestDiff

---
+++ austin_tx/1-6-21/threeNumberSum.js
@@ -0,0 +1,23 @@
+const threeNumberSum = (array, targetSum) => {
+  const triplets = [];
+  array.sort((a, b) => a - b);
+  for (let i = 0; i < array.length - 2; i++) {
+    let left = i + 1;
+    let right = array.length - 1;
+
+    while (left < right) {
+      let currentSum = array[i] + array[left] + array[right];
+
+      if (currentSum === targetSum) {
+        triplets.push([array[i], array[left], array[right]]);
+        left++;
+        right--;
+      } else if (currentSum < targetSum) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return triplets;
+};
