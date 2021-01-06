39dc9262c5b36583e67b70d574737bf0a902f6f8
dcampuzano101 <dcampuzano101@gmail.com>
2021-01-06 11:06:14 -0600

    redo - algoexpert - medium - longestPeak/arrayOfProducts/firstDuplicateValue

--- austin_tx/1-6-21/longestPeak.js
+++ austin_tx/1-6-21/longestPeak.js
@@ -1,1 +1,30 @@
-const longestPeak = (array) => {};
+const longestPeak = (array) => {
+  let maxPeak = 0;
+  let i = 1;
+  while (i < array.length - 1) {
+    const isPeak = array[i - 1] < array[i] && array[i + 1] < array[i];
+
+    if (!isPeak) {
+      i++;
+      continue;
+    }
+
+    let leftIndex = i - 2;
+
+    while (leftIndex >= 0 && array[leftIndex] < array[leftIndex + 1]) {
+      leftIndex--;
+    }
+    let rightIndex = i + 1;
+    while (
+      rightIndex < array.length &&
+      array[rightIndex] < array[rightIndex - 1]
+    ) {
+      rightIndex++;
+    }
+
+    const currentPeakLength = rightIndex - leftIndex - 1;
+    maxPeak = Math.max(maxPeak, currentPeakLength);
+    i = rightIndex;
+  }
+  return maxPeak;
+};
