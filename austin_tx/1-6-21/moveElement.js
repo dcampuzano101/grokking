b89344c0a77013a717cbb2748e6fe72345903b69
dcampuzano101 <dcampuzano101@gmail.com>
2021-01-06 09:52:33 -0600

    redo - algoexpert - medium - moveElementToEnd

---
+++ austin_tx/1-6-21/moveElement.js
@@ -0,0 +1,17 @@
+const moveElementToEnd = (array, target) => {
+  // two pointers
+  let i = 0;
+  let j = array.length - 1;
+
+  while (i < j) {
+    while (i < j && array[j] === target) j--;
+
+    if (array[i] === target) {
+      let swap = array[i];
+      array[i] = array[j];
+      array[j] = swap;
+    }
+    i++;
+  }
+  return array;
+};
